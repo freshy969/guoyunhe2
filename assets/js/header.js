fdf4e4654e6a477619a4c280535131bd5b4695cf
Guo Yunhe <guoyunhebrave@gmail.com>
2018-04-02 10:28:28 +0300

    Featured image header

--- assets/js/header.js
+++ assets/js/header.js
@@ -8,3 +8,12 @@ BackgroundCheck.init({
 	targets: ".site-navigation, .site-branding-text, .menu-scroll-down",
 	images: ".wp-custom-header img"
 });
+
+var $featuredImage = jQuery(".single-featured-image-header img");
+
+if ($featuredImage.length) {
+	$featuredImage.height(Math.max($featuredImage.width() * 0.3333, 300));
+	jQuery(window).resize(function() {
+		$featuredImage.height(Math.max($featuredImage.width() * 0.3333, 300));
+	});
+}
