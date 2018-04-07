0947042ad526795ef5ca87549aa7521b00959984
Guo Yunhe <guoyunhebrave@gmail.com>
2018-04-07 11:27:31 +0300

    Remove color partten including

--- functions.php
+++ functions.php
@@ -308,23 +308,6 @@ function guoyunhe2_pingback_header() {
 add_action( 'wp_head', 'guoyunhe2_pingback_header' );
 
 /**
- * Display custom color CSS.
- */
-function guoyunhe2_colors_css_wrap() {
-	if ( 'custom' !== get_theme_mod( 'colorscheme' ) && ! is_customize_preview() ) {
-		return;
-	}
-
-	require_once( get_parent_theme_file_path( '/inc/color-patterns.php' ) );
-	$hue = absint( get_theme_mod( 'colorscheme_hue', 250 ) );
-?>
-	<style type="text/css" id="custom-theme-colors" <?php if ( is_customize_preview() ) { echo 'data-hue="' . $hue . '"'; } ?>>
-		<?php echo guoyunhe2_custom_colors_css(); ?>
-	</style>
-<?php }
-add_action( 'wp_head', 'guoyunhe2_colors_css_wrap' );
-
-/**
  * Enqueue scripts and styles.
  */
 function guoyunhe2_scripts() {
